d6d8eeef34b57846d2dc9da08a9a225a01d24a97
Lucas Imroth <lucas-imroth@outlook.com.br>
2026-06-07 10:05:23 -0300

    atualizacao da organização de codigo, padronizacao dos dados e ajustes

--- notebooks/selecao.py
+++
@@ -1,7 +0,0 @@
-from sklearn.feature_selection import SelectKBest, f_classif
-
-def filter_select(X, y, k=10):
-    """Exemplo simples de seleção filter usando ANOVA F-test."""
-    sel = SelectKBest(f_classif, k=min(k, X.shape[1]))
-    Xs = sel.fit_transform(X, y)
-    return Xs, sel
